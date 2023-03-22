37a0a730b9bc28eadb9b5347760d96464b9a6298
Vitor Faruk <75445536+VitorFaruk@users.noreply.github.com>
2023-03-21 21:29:12 -0300

    Create Exercicio4_1.py

---
+++ PDC/Aula04/Exercicio4_1.py
@@ -0,0 +1,9 @@
+# Exercício 4.1-) Crie um algoritmo que imprima na tela os
+# números de 1 a 20, um abaixo do outro.
+
+# Abertura do laço de repetição com tamanho
+for i in range(20):
+    # Somatória no contador
+    i = i + 1
+    # Impressão do resultado
+    print(i)
