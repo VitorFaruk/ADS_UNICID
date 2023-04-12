6e806348b254d2f3af5496b466f33efbee626241
Vitor Faruk <75445536+VitorFaruk@users.noreply.github.com>
2023-04-11 21:37:58 -0300

    Create Aula07.py

---
+++ PDC/Aula07/Codigo/Aula07.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o valor de A:"))
+b = int(input("Digite o valor de B:"))
+
+if a > b:
+    print("O valor de A é maior que B")
+if b > a:
+    print("O valor de B é maior que A")
+if a == b:
+    print("Os valores são iguais")
