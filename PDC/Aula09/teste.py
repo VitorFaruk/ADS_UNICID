e7151ded8087ea97d73c468cd0076f81e43d9978
Vitor Faruk <75445536+VitorFaruk@users.noreply.github.com>
2023-04-25 20:27:19 -0300

    Create teste.py

---
+++ PDC/Aula09/teste.py
@@ -0,0 +1,15 @@
+# Aula 09 (Programação de computadores) - String
+
+palavra = "corinthians"
+upper = palavra.upper()
+print("Imprime o caractere correspondente a posição zero: ", palavra[0])
+
+print("Imprime o caractere correspondente a última posição: ", palavra[-1])
+
+print("Imprime os caracteres desde a posição 0 até a 3: ", palavra[0:3])
+
+print("Imprime todos os caracteres até a posição 5: ", palavra[:5])
+
+print("Imprime todos os caracteres a partir da posição 5: ", palavra[5:])
+
+print("Imprime a quantidade de caracteres: ", len(palavra))
